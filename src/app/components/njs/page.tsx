047a8c5f0eb9c5b9a16a3d74aa4b0dc78aa890a5
SeOinm <anatjdls0716@gmail.com>
2025-07-14 08:52:55 +0000

    💄 style: day-item 컴포넌트 구현

--- src/app/components/njs/page.tsx
+++ src/app/components/njs/page.tsx
@@ -1,13 +1,14 @@
+import DayItem from "@/components/ui/day-item";
+
 export default function ComponentPage() {
   return (
-    <div className="flex flex-col gap-4 items-center">
-      <div>
-        <h2 className="mb-2">컴포넌트 명</h2>
-        <div></div>
-      </div>
-      <div>
-        <h2 className="mb-2">컴포넌트 명</h2>
-        <div></div>
+    <div className="flex flex-col gap-4 items-center bg-bg min-h-screen py-10">
+      <div className="w-full max-w-2xl mx-auto">
+        <h2 className="mb-2 text-16 font-bold">DayItem</h2>
+        <div className="space-y-4 w-full">
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
+          <DayItem empty />
+        </div>
       </div>
     </div>
   );
