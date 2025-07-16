d2d268d330f3a54d1896fe32acb6586c81fc507e
SeOinm <anatjdls0716@gmail.com>
2025-07-16 00:31:09 +0000

    💄 style: 마이페이지 레이아웃 구성

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,4 +1,23 @@
-// 마이페이지
+import ProfileCard from "@/components/ui/profileItem";
+import DayItem from "@/components/ui/day-item";
+import SelectMenu2 from "@/components/ui/selectMenu2";
+
 export default function MypagePage() {
-  return <h2>마이페이지</h2>;
+  return (
+    <div className="flex flex-col gap-8 items-center">
+      <div className="w-full">
+        <ProfileCard userName="여행덕후"></ProfileCard>
+      </div>
+      <div className="w-full">
+        <SelectMenu2 />
+      </div>
+      <div className="w-full">
+        <div className="space-y-4 w-full">
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
+          <DayItem name="제주도" period="2025.07.12 ~ 2025.07.15." dday="D-2" />
+        </div>
+      </div>
+    </div>
+  );
 }
