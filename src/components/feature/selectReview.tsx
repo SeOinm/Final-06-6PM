5e67ec091e983e42a0bc155c82b4ba419961e620
SeOinm <anatjdls0716@gmail.com>
2025-07-28 08:35:51 +0000

    🔥 remove: 브루노 테스트용 조회 삭제

--- src/components/feature/selectReview.tsx
+++ src/components/feature/selectReview.tsx
@@ -1,50 +1,59 @@
 "use client";
 
-import { CalendarDays, LayoutList, MapPin } from "lucide-react";
 import { useEffect, useState } from "react";
-import ViewItem from "./viewItem";
 import useUserStore from "@/zustand/userStore";
+import { useSearchParams } from "next/navigation";
+
 import { GetReviewDetailProps } from "@/types/review";
 import { getReviewAllUser, getReviewDailyUser, getReviewPlaceUser } from "@/lib/api/review";
-interface FetchProps {
+import { CalendarDays, LayoutList, MapPin } from "lucide-react";
+import ViewItem from "./viewItem";
+
+// 리뷰 타입별 요청
+interface ReviewTypeProps {
   all: boolean;
   daily: boolean;
   place: boolean;
 }
 
 export default function SelectReview() {
-  const [tab, setTab] = useState(0);
   const token = useUserStore((state) => state.token);
+  const searchParams = useSearchParams();
+  const planIdParam = searchParams.get("plan_id");
+
+  const [tab, setTab] = useState(0);
   const [reviewAll, setReviewAll] = useState<GetReviewDetailProps[]>([]);
   const [reviewDaily, setReviewDaily] = useState<GetReviewDetailProps[]>([]);
   const [reviewPlace, setReviewPlace] = useState<GetReviewDetailProps[]>([]);
-  const [fetched, setFetched] = useState<FetchProps>({
+
+  const [reviewType, setReviewType] = useState<ReviewTypeProps>({
     all: false,
     daily: false,
     place: false,
   });
 
+  // 탭에 해당하는 리뷰데이터 요청
   useEffect(() => {
     if (!token) return;
 
     const fetchData = async () => {
-      if (tab === 0 && !fetched.all) {
+      if (tab === 0 && !reviewType.all) {
         const res = await getReviewAllUser(token);
         if (res.ok) {
           setReviewAll(res.item);
-          setFetched((prev) => ({ ...prev, all: true }));
+          setReviewType((prev) => ({ ...prev, all: true }));
         }
-      } else if (tab === 1 && !fetched.daily) {
+      } else if (tab === 1 && !reviewType.daily) {
         const res = await getReviewDailyUser(token);
         if (res.ok) {
           setReviewDaily(res.item);
-          setFetched((prev) => ({ ...prev, daily: true }));
+          setReviewType((prev) => ({ ...prev, daily: true }));
         }
-      } else if (tab === 2 && !fetched.place) {
+      } else if (tab === 2 && !reviewType.place) {
         const res = await getReviewPlaceUser(token);
         if (res.ok) {
           setReviewPlace(res.item);
-          setFetched((prev) => ({ ...prev, place: true }));
+          setReviewType((prev) => ({ ...prev, place: true }));
         }
       }
     };
@@ -73,6 +82,11 @@ export default function SelectReview() {
     },
   ];
 
+  // 쿼리스트링(plan_id)이 있으면 필터링
+  const filterReview = planIdParam
+    ? tabData[tab].description.filter((item) => item.extra.plan_id === Number(planIdParam))
+    : tabData[tab].description;
+
   return (
     <div>
       <div className="grid grid-cols-3 divide-x divide-travel-gray100">
@@ -81,7 +95,7 @@ export default function SelectReview() {
             key={item.id}
             className={`text-14 flex flex-col items-center p-1.5 gap-1.5 cursor-pointer ${
               tab === item.id
-                ? "text-white bg-travel-secondary100  border-b border-b-travel-secondary200"
+                ? "text-white bg-travel-secondary100 border-b border-b-travel-secondary200"
                 : "text-travel-gray400 bg-white border-b border-b-travel-gray200"
             }`}
             onClick={() => setTab(item.id)}
@@ -92,9 +106,9 @@ export default function SelectReview() {
         ))}
       </div>
 
-      <div className="space-y-4 p-4">
-        {tabData[tab].description.length > 0 ? (
-          tabData[tab].description.map((item) => <ViewItem key={item._id} {...item} />)
+      <div className="space-y-4 p-4 min-h-25 content-center">
+        {filterReview.length > 0 ? (
+          filterReview.map((item) => <ViewItem key={item._id} {...item} />)
         ) : (
           <div className="text-center text-travel-gray300 text-sm">리뷰가 없습니다.</div>
         )}
---
+++ src/components/feature/selectReviewTab.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import useUserStore from "@/zustand/userStore";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { getPlanListUser } from "@/lib/api/plan";
+import { GetPlanDetailProps } from "@/types/plan";
+import { ChevronDown } from "lucide-react";
+
+export default function SelectReviewTab() {
+  const token = useUserStore((state) => state.token);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [userPlan, setUserPlan] = useState<GetPlanDetailProps[]>([]);
+  const [selectPlan, setSelectPlan] = useState<string>(searchParams.get("plan_id") || "");
+
+  // 사용자 여행일정 전체 조회
+  useEffect(() => {
+    const UserPlanData = async () => {
+      if (!token) return;
+      try {
+        const res = await getPlanListUser(token);
+        if (res.ok) {
+          setUserPlan(res.item);
+        }
+      } catch (error) {
+        console.error("계획 데이터 로드 실패:", error);
+      }
+    };
+
+    UserPlanData();
+  }, [token]);
+
+  // 셀렉트박스 변경 핸들러
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setSelectPlan(value);
+    const query = value ? `?plan_id=${value}` : "";
+    router.replace(`/mypage/review${query}`);
+  };
+
+  return (
+    <div className="relative mb-2">
+      <label htmlFor="daily-review" className="sr-only">
+        일정선택
+      </label>
+      <select
+        name="daily-review"
+        id="daily-review"
+        value={selectPlan}
+        onChange={handleSelect}
+        className="w-full border border-travel-gray400 py-3 px-4 rounded-lg 
+            bg-white text-travel-text100 text-14
+              focus:outline-none focus:border-travel-primary100 focus:ring-2 focus:ring-travel-primary100
+              hover:border-travel-primary100
+              appearance-none cursor-pointer"
+      >
+        <option value="" className="text-travel-gray500">
+          필터링하고 싶은 일정을 선택하세요
+        </option>
+        {userPlan.map((plan) => (
+          <option key={plan._id} value={String(plan._id)} className="py-2">
+            {plan.title} ({plan.extra.startDate} ~ {plan.extra.endDate})
+          </option>
+        ))}
+      </select>
+      <div className="absolute top-4 right-0 flex items-center px-3 pointer-events-none">
+        <ChevronDown className="w-4 h-4 text-travel-gray600" />
+      </div>
+    </div>
+  );
+}
